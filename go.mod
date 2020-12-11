18b50e9d9b0975cca4721f55e8a7d4c071ff94d9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-12-11 18:32:28 +0900

    Use `systray`

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/y-yagi/cli-for-windows
 
 go 1.15
+
+require github.com/getlantern/systray v1.1.0
--- main.go
+++ main.go
@@ -4,14 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/getlantern/systray"
 )
+
+var server *http.Server
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world")
 	})
 
-	if err := http.ListenAndServe(":3000", nil); err != nil {
-		log.Fatal(err)
-	}
+	server = &http.Server{Addr: ":3000"}
+	go func(server *http.Server) {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}(server)
+	systray.Run(onReady, onExit)
+
+	return
+}
+
+func onReady() {
+	systray.SetTitle("My CLI")
+	systray.SetTooltip("My CLI")
+	mQuit := systray.AddMenuItem("Quit", "Quit")
+	<-mQuit.ClickedCh
+	systray.Quit()
+}
+
+func onExit() {
+	server.Close()
 }
